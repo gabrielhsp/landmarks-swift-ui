b7e3ae2167463c8fa4d89dbf32c37906ced3e9c2
Gabriel Henrique <gabhenrique0@gmail.com>
2020-09-18 19:31:40 -0300

    Chore: create first part of project with the base views

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gabriel Pereira on 18/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
