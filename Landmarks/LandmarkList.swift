a5f345406db7403f6420dedc2b2f0bd3ee8424f1
Gabriel Henrique <gabhenrique0@gmail.com>
2020-09-19 15:47:27 -0300

    Feat: add a dynamic list from a mocked JSON with navigation between screens

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Gabriel Pereira on 19/09/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 11 Pro Max", "iPhone SE (2nd generation)", "iPad Air (3rd generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
