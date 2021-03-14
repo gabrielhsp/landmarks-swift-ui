ef3ea52abff19168b778340185f447eefbf7eddc
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-14 13:48:32 -0300

    :pencil2: Feat: add identifiable protocol on landmark

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     // MARK: - Public Properties
     var id: Int
     var name: String
