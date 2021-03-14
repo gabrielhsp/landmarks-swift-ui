eb38e22a69ce05672bebfcc7b63122aa910e91e4
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-14 13:24:04 -0300

    :pencil2: Feat: add stored properties on landmark model

--- Landmarks/Landmarks/Landmark.swift
+++ Landmarks/Landmarks/Landmark.swift
@@ -6,11 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
+    // MARK: - Public Properties
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    
+    // MARK: - Private Properties
+    private var imageName: String
+    private var coordinates: Coordinates
+    
+    // MARK: - Stored Properties
+    var image: Image {
+        Image(imageName)
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
 }
