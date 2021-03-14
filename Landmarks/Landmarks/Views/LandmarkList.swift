c7cf24fdc184347b93f79bc6de3aa8f353563e4c
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-14 13:47:57 -0300

    :sparkles: Feat: create a landmark list view

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Gabriel Henrique on 14/03/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
