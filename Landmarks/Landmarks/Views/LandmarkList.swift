efffa7d4a2c5c5ce6c83a153b9073c5038136581
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-20 17:41:09 -0300

    :sparkles: Feat: add logic to show only favorites landmarks
    Add a logic to only show the favorites landmarks on landmark list

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly: Bool = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
             }
+            .navigationTitle("Landmarks")
         }
-        .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
+        LandmarkList()
     }
 }
