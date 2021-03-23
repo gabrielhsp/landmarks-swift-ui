f07ffdfb35392ac0da1885073ce53612e1220861
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-22 21:41:34 -0300

    :sparkles: Feat: create categories group and category view

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Gabriel Henrique on 22/03/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
