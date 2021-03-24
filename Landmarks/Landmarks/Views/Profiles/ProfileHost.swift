e5543a10d040dc392cea8ba382b4c8a7791b897c
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-24 20:46:13 -0300

    :sparkles: Feat: add profile on model data object

--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ProfileHost: View {
     @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile: Profile = .default
     
     var body: some View {
@@ -18,7 +19,11 @@ struct ProfileHost: View {
                 EditButton()
             }
             
-            ProfileSummary(profile: draftProfile)
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
