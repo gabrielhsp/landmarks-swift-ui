9d1c46a40094b8ec09320da410f6c723f3902b55
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-29 20:08:30 -0300

    :sparkles: Feat: create page view controller

---
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Gabriel Henrique on 29/03/21.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController: UIPageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])],
+            direction: .forward,
+            animated: true
+        )
+    }
+}
