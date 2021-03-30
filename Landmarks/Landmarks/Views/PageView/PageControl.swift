b83b5e102afc03e074037e6ac9b9cc6bd462fad5
Gabriel Henrique <gabhenrique0@gmail.com>
2021-03-29 21:10:30 -0300

    :lipstick: Feat: create page control view

---
+++ Landmarks/Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,25 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Gabriel Henrique on 29/03/21.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control: UIPageControl = UIPageControl()
+            control.numberOfPages = numberOfPages
+        
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
